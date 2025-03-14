8bdac50ad4c7d9202fc7cb6365fbce43612ce59f
IsidoraSlavkovic <isidora.slavkovic@gmail.com>
2025-03-14 20:07:18 +0100

    Save only depth, normals and rgb.

--- run_slurm.sh
+++ run_slurm.sh
@@ -15,7 +15,7 @@ module load eth_proxy
 name=pano-infinigen-array100
 # id=${SLURM_JOB_ID} #03790512 #02691156 #03790512 #02958343
 num_scenes=1
-num_concurrent=16
+num_concurrent=32
 folder_name="${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
 
 python -m infinigen.datagen.manage_jobs --output_folder outputs/indoor/$folder_name --num_scenes $num_scenes --pipeline_configs local_256GB.gin monocular.gin blender_gt.gin indoor_background_configs.gin \
